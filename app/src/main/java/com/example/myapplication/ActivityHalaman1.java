8bd54ac960080337c85e7168ad89e18929f586ad
InshiatunHanifah <insiatunhanifah@gmail.com>
2021-09-30 15:00:33 +0700

    update layout main

---
+++ app/src/main/java/com/example/myapplication/ActivityHalaman1.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.INotificationSideChannel;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ActivityHalaman1 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman1);
+
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.myapplication;
 
+import static com.example.myapplication.R.drawable.banana;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.ActivityHalaman1;
+import com.example.myapplication.ActivityHalaman2;
+import com.example.myapplication.ActivityHalaman3;
+import com.example.myapplication.ActivityHalaman4;
+import com.example.myapplication.R;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivhalaman1, ivhalaman2, ivhalaman3, ivhalaman4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivhalaman1 = findViewById(R.id.iv_halaman1);
+        ivhalaman1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, ActivityHalaman1.class);
+                startActivity(intent);
+            }
+        });
+
+            ivhalaman2 = findViewById(R.id.iv_halaman2);
+            ivhalaman2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent( MainActivity.this, ActivityHalaman2.class);
+                    startActivity(intent);
+                }
+            });
+        ivhalaman3 = findViewById(R.id.iv_halaman3);
+        ivhalaman3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, ActivityHalaman3.class);
+                startActivity(intent);
+            }
+        });
+        ivhalaman4 = findViewById(R.id.iv_halaman4);
+        ivhalaman4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this, ActivityHalaman4.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
